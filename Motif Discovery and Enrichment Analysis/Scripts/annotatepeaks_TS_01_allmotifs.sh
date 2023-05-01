ddc5e0040d3d05addca02b482dd8335ca06e3e39
ant0028 <114441666+ant0028@users.noreply.github.com>
2023-05-01 18:19:26 -0500

    Update and rename annotatpeaks_TS_01_allmotifs.sh to annotatepeaks_TS_01_allmotifs.sh

---
+++ Motif Discovery and Enrichment Analysis/Scripts/annotatepeaks_TS_01_allmotifs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#An example of using annotate peaks to create a histogram of relative location of motifs to peak. This uses the files with all motifs, 
+#but you can use this for a specific motif by extracting from the discovery results or creating your own motif file as described on Homer's website
+#The output can be graphed in excel
+annotatepeaks.pl ./peakcalls/TS_01.bam_summits.bed Fence_Lizard -m ./TS_01.bam_summits/1homerMotifs.all.motifs -size 1000 -hist 10 > TS_01_annotatepeaks_hist.txt
